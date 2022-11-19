67284a67c4cf90e3826e7851c4cf27a57696a0df
Jafar1i <103290548+Jafarl1@users.noreply.github.com>
2022-11-19 14:47:13 +0400

    loader added

--- dms/src/App.jsx
+++ dms/src/App.jsx
@@ -61,9 +61,6 @@ function App() {
   };
 
 
-
-
-
   return (
     <div className="App">
       {
--- dms/src/components/Loader.jsx
+++ dms/src/components/Loader.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ClipLoader } from 'react-spinners'
 
 function Loader() {
-  
+
   return (
-    <div>Loader</div>
+    <div className="loader">
+      <ClipLoader color="#5d5d5d" size={50} />
+    </div>
   )
 }
 
